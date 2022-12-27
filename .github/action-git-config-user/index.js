3f8a28d84858e2cf04b03a7f15586dc9fea1712d
Justin Klaassen <6700251+jck112@users.noreply.github.com>
2022-12-27 14:45:18 -0800

    Remove logs

--- .github/action-git-config-user/index.js
+++ .github/action-git-config-user/index.js
@@ -5,19 +5,11 @@ const { graphql } = require("@octokit/graphql");
 async function run() {
   try {
     // Fetch authenticated user info using GraphQL API.
-    core.startGroup("Fetching user info");
     const token = core.getInput("token");
     let { viewer: { login, name, email, databaseId } } = await graphql({
       query: `{ viewer { login name email databaseId } }`,
       headers: { authorization: `bearer ${token}` },
     });
-    core.info(`
-      login: ${login}
-      name: ${name}
-      email: ${email}
-      databaseId: ${databaseId}
-    `)
-    core.endGroup();
 
     // Fallback to "ID+USERNAME@users.noreply.github.com".
     name ||= login;

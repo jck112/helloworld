3504facbeac475a7b6e32dc5fe4bb4241b41b3b2
Justin Klaassen <6700251+jck112@users.noreply.github.com>
2022-12-27 14:38:58 -0800

    Add some logs

--- .github/action-git-config-user/index.js
+++ .github/action-git-config-user/index.js
@@ -5,19 +5,27 @@ const { graphql } = require("@octokit/graphql");
 async function run() {
   try {
     // Fetch authenticated user info using GraphQL API.
+    core.startGroup("Fetching user info");
     const token = core.getInput("token");
     let { viewer: { login, name, email, databaseId } } = await graphql({
       query: `{ viewer { login name email databaseId } }`,
       headers: { authorization: `bearer ${token}` },
     });
+    core.info(`
+      login: ${login}
+      name: ${name}
+      email: ${email}
+      databaseId: ${databaseId}
+    `)
+    core.endGroup();
 
     // Fallback to "ID+USERNAME@users.noreply.github.com".
     name ||= login;
     email ||= `${databaseId}+${login}@users.noreply.github.com`;
 
     // Set name and email using "git config".
-    await exec.exec(`git config --global user.name '${name}'`);
-    await exec.exec(`git config --global user.email '${email}'`);
+    await exec.exec(`git config --global user.name ${name}`);
+    await exec.exec(`git config --global user.email ${email}`);
   } catch (error) {
     core.setFailed(error.message);
   }

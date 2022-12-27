0e6f670398cd4dd4ae9c1c874e12f48c28ebe6b3
Justin Klaassen <6700251+jck112@users.noreply.github.com>
2022-12-27 12:33:43 -0800

    Try to read properties

--- .github/action-git-config-user/index.js
+++ .github/action-git-config-user/index.js
@@ -4,15 +4,16 @@ const { graphql } = require("@octokit/graphql");
 async function run() {
   try {
     const token = core.getInput("token");
-    const { result } = await graphql({
+    const { viewer } = await graphql({
       query: `{ viewer { login name email databaseId } }`,
       headers: { authorization: `bearer ${token}` }
     });
-    let login = result.viewer.login
-    let name = result.viewer.name
-    let email = result.viewer.email
-    let id = result.viewer.databaseId
-    console.log(`The query response: ${login} ${name} ${email} ${id}`);
+    let { login, name, email, databaseId } = viewer
+    // let login = result.viewer.login
+    // let name = result.viewer.name
+    // let email = result.viewer.email
+    // let id = result.viewer.databaseId
+    console.log(`The query response: ${login} ${name} ${email} ${databaseId}`);
   } catch (error) {
     core.setFailed(error.message);
   }

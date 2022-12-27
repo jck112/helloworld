cb76627c131d457fed10c2eddf02a0977c5a22df
Justin Klaassen <6700251+jck112@users.noreply.github.com>
2022-12-27 14:12:34 -0800

    Refactor destructuring of query response

--- .github/action-git-config-user/index.js
+++ .github/action-git-config-user/index.js
@@ -3,14 +3,18 @@ const { graphql } = require("@octokit/graphql");
 
 async function run() {
   try {
+    // Fetch authenticated user info using GraphQL API.
     const token = core.getInput("token");
-    const { viewer } = await graphql({
+    let { viewer: { login, name, email, databaseId } } = await graphql({
       query: `{ viewer { login name email databaseId } }`,
-      headers: { authorization: `bearer ${token}` }
+      headers: { authorization: `bearer ${token}` },
     });
-    const name = viewer.name || viewer.login
-    const email = viewer.email || `${viewer.databaseId}+${viewer.login}@users.noreply.github.com`
-    console.log(`The query response: ${name} <${email}>`);
+
+    // Fallback to "ID+USERNAME@users.noreply.github.com".
+    name ||= login;
+    email ||= `${databaseId}+${login}@users.noreply.github.com`;
+
+    console.log(`${name} <${email}>`)
   } catch (error) {
     core.setFailed(error.message);
   }

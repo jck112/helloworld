3f4f67f74de680c64ef41fad4e553f67b487af4c
Justin Klaassen <6700251+jck112@users.noreply.github.com>
2022-12-27 11:28:19 -0800

    Wrap with async function

--- .github/action-git-config-user/index.js
+++ .github/action-git-config-user/index.js
@@ -1,13 +1,17 @@
 const core = require('@actions/core');
 const { graphql } = require("@octokit/graphql");
 
-try {
-  const token = core.getInput("token");
-  const { response } = await graphql({
-    query: `{ viewer { login name email databaseId } }`,
-    headers: { authorization: `bearer ${token}` }
-  });
-  console.log(`The query response: ${response}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const token = core.getInput("token");
+    const { response } = await graphql({
+      query: `{ viewer { login name email databaseId } }`,
+      headers: { authorization: `bearer ${token}` }
+    });
+    console.log(`The query response: ${response}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();

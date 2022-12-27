cb694e1a76f9988f9b094d55d60adf14de837725
Justin Klaassen <6700251+jck112@users.noreply.github.com>
2022-12-27 11:36:32 -0800

    Try different query syntax

--- .github/action-git-config-user/index.js
+++ .github/action-git-config-user/index.js
@@ -4,10 +4,10 @@ const { graphql } = require("@octokit/graphql");
 async function run() {
   try {
     const token = core.getInput("token");
-    const { response } = await graphql({
-      query: `{ viewer { login name email databaseId } }`,
-      headers: { authorization: `bearer ${token}` }
-    });
+    const { response } = await graphql(
+      `{ viewer { login name email databaseId } }`,
+      { headers: { authorization: `bearer ${token}` } }
+    );
     console.log(`The query response: ${response}`);
   } catch (error) {
     core.setFailed(error.message);

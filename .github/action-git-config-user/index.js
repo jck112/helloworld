1f14d9dc1a3f0b583567dab0efac3db44ca9c02b
Justin Klaassen <6700251+jck112@users.noreply.github.com>
2022-12-27 11:21:49 -0800

    Use CommonJS modules

--- .github/action-git-config-user/index.js
+++ .github/action-git-config-user/index.js
@@ -1,13 +1,13 @@
-import { getInput, setFailed } from "@actions/core";
-import { graphql } from "@octokit/graphql";
+const core = require('@actions/core');
+const { graphql } = require("@octokit/graphql");
 
 try {
-  const token = getInput("token");
+  const token = core.getInput("token");
   const { response } = await graphql({
     query: `{ viewer { login name email databaseId } }`,
     headers: { authorization: `bearer ${token}` }
   });
   console.log(`The query response: ${response}`);
 } catch (error) {
-  setFailed(error.message);
+  core.setFailed(error.message);
 }
